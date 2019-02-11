f8e0f1e38a1f2eca90b69d74f7e61dee796e216c
hazwatt <rouver.baptiste@gmail.com>
2019-02-11 17:06:47 +0100

    tableau

---
+++ 06-tableaux/tableaux.php
@@ -0,0 +1,33 @@
+<?php
+$eleves = [
+    0 => [
+        'nom' => 'Matthieu',
+        'notes' => [10, 8, 16, 20, 17, 16, 15, 2]
+    ],
+    1 => [
+        'nom' => 'Thomas',
+        'notes' => [4, 18, 12, 15, 13, 7]
+    ],
+    2 => [
+        'nom' => 'Jean',
+        'notes' => [17, 14, 6, 2, 16, 18, 9]
+    ],
+    3 => [
+        'nom' => 'Enzo',
+        'notes' => [1, 14, 6, 2, 1, 8, 9]
+    ]
+];
+foreach($eleves as $eleve){
+   echo $eleve['nom'];
+   echo ' a eu <br/>';
+   foreach($eleve['notes'] as $note){
+       if(count(-1)){
+           $v='et';
+       }else{
+           $v=',';
+       }
+       echo $note.$v;
+   }
+}
+
+
